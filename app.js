0043e774c65db61b64d10758ed133c511ed3f44e
Adarsha A <adarshaadi1997@gmail.com>
2023-07-20 12:50:26 +0530

    Roter aand Routing

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
-
 const bodyParser = require("body-parser");
+
+const adminRoutes = require("./Routes/admin");
+const shopRouetr = require("./Routes/shop");
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use("/add-products", (req, res, next) => {
-//   console.log("In middleware 2");
-  res.send(
-    `<form action="/product" method="POST"><input type="text" name="title" /><input type="password" name="password" /><button type="submit">submit</button></form>`
-  );
-});
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-app.use("/", (req, res, next) => {
-//   console.log("In middleware /");
-  res.send(`<h1>Hello World</h1>`);
-});
+app.use(adminRoutes);
+app.use(shopRouetr);
 
-// const server = http.createServer(app);
 
-// server.listen(3000);
 app.listen(3000);
