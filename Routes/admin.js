7deeaba42eabf24b7f9d133586182e4f980ecfa5
Adarsha A <adarshaadi1997@gmail.com>
2023-07-21 12:54:04 +0530

    Css changes(not importent)

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,15 +1,20 @@
-const express = require('express');
-const router = express.Router();
 const path = require('path');
+
+const express = require('express');
+
 const rootDir = require('../util/path');
 
-router.get("/add-product", (req, res, next) => {
-  //   console.log("In middleware 2");
-  res.sendFile(path.join(rootDir,'views','product.html'))
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
-router.post("/product", (req, res, next) => {
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
-  res.redirect("/");
+  res.redirect('/');
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const bodyParser = require("body-parser");
 const path = require('path');
-const rootDir = require('./util/path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-const adminRoutes = require("./Routes/admin");
-const shopRouetr = require("./Routes/shop");
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRoutes);
-app.use(shopRouetr);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use((req, res, next)=>{
-    res.status(404).sendFile(path.join(rootDir, 'views','pageNotFound.html'));
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-
 
 app.listen(3000);
