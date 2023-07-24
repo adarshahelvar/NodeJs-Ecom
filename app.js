10792df73c438e9fdb39b0f1a80b751bf382c988
Adarsha A <adarshaadi1997@gmail.com>
2023-07-24 12:25:27 +0530

    Added new fields for header and cart

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 
-const errorController = require('./controllers/error'); // Make sure you import the errorController
+const errorController = require('./controllers/error');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -17,8 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.get404); // This handles the 404 error and renders the 404 page with the path variable
+app.use(errorController.get404);
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+app.listen(3000);
