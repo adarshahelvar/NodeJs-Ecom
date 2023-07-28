842d075fbb1c7ab5e9aa896a12e23e46095f0fa6
Adarsha A <adarshaadi1997@gmail.com>
2023-07-28 17:06:29 +0530

    Product added to DB using sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,15 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-  })
+  // req.user.createProduct  we are getting from sequlize;
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+      userId: req.user.id,
+    })
     .then((result) => {
       // console.log(result);
       console.log("Created Product");
@@ -86,19 +89,17 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.postDeleteProduct = (req, res, next) => {
-const prodId = req.body.productId;
-Product.findByPk(prodId)
-  .then((product) => {
-    return product.destroy();
-  })
-  .then((res) => {
-    console.log("Product deleted");
-    res.redirect("/admin/products");
-  })
-  .catch((err) => {
-    console.log(err)
-    res.redirect('/admin/products');
-  });
+  const prodId = req.body.productId;
+  Product.findByPk(prodId)
+    .then((product) => {
+      return product.destroy();
+    })
+    .then((res) => {
+      console.log("Product deleted");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/admin/products");
+    });
 };
-
-
