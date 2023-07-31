57347cb6d65c8868fb86d036c4b75ebcdac1c15d
Adarsha A <adarshaadi1997@gmail.com>
2023-07-31 13:07:30 +0530

    Cart and previous order page updated

--- app.js
+++ app.js
@@ -1,71 +1,74 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const errorController = require("./controllers/error");
-const sequeize = require("./util/database");
-const Product = require("./models/product");
-const User = require("./models/user");
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   User.findByPk(1)
-    .then((user) => {
+    .then(user => {
       req.user = user;
       next();
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 });
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
+Product.belongsToMany(Order, { through: OrderItem });
 
-sequeize
+
+sequelize
   // .sync({ force: true })
-  // If force is always true then it will always override our DB table
-  sequeize.sync()
-  .then((result) => {
+  .sync()
+  .then(result => {
     return User.findByPk(1);
-    // console.log(result)
+    // console.log(result);
   })
-  .then((user) => {
+  .then(user => {
     if (!user) {
-      User.create({ name: "Adi", email: "test@example.com" });
+      return User.create({ name: 'Max', email: 'test@test.com' });
     }
     return user;
   })
-  .then((user) => {
+  .then(user => {
     // console.log(user);
     return user.createCart();
+  })
+  .then(cart => {
     app.listen(3000);
   })
-  .then((cart) => {
-    app.listen(3000);
-  })
-  .catch((err) => {
+  .catch(err => {
     console.log(err);
   });
-
-// app.listen(3000);
